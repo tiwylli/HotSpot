Fix numerical error in polygon gt computation.

dataset/shape_2d.py
@@ -170,7 +170,7 @@ class Polygon(ShapeBase):
 
         d1 = np.linalg.norm(pp1, axis=0)
         d2 = np.linalg.norm(pp2, axis=0)
-        dp = np.sqrt(np.square(d1) - np.square(r * np.array(self.lines["line_length"])[:, None]))
+        dp = np.sqrt(np.max(np.square(d1) - np.square(r * np.array(self.lines["line_length"])[:, None]), 0))
         d = np.where(r < 0, d1, np.where(r > 1, d2, dp))
         distances = np.min(d, axis=0)
         idx = np.argmin(d, axis=0)

train/train.py
@@ -127,8 +127,13 @@ def visualize_model(
         mae = np.mean(np.abs(vis_grid_pred_np - vis_grid_dists_gt_np))
         rmse = np.sqrt(np.mean((vis_grid_pred_np - vis_grid_dists_gt_np) ** 2))
         mape = np.mean(np.abs(vis_grid_pred_np - vis_grid_dists_gt_np) / vis_grid_dists_gt_np)
+        smape = np.mean(
+            2
+            * np.abs(vis_grid_pred_np - vis_grid_dists_gt_np)
+            / (np.abs(vis_grid_pred_np) + np.abs(vis_grid_dists_gt_np))
+        )
         utils.log_string(
-            f"RMSE: {rmse:.5f}, MAE: {mae:.5f}, MAPE: {mape:.5f}", log_file
+            f"RMSE: {rmse:.10f}, MAE: {mae:.10f}, MAPE: {mape:.10f}, SMAPE: {smape:.10f}", log_file
         )
 
     utils.log_string("", log_file)