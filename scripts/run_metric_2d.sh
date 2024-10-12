8edd9e4ef8bef3a4151af44aec491a0c03cd9558
ZeamoxWang <wangzimo@zju.edu.cn>
2024-10-12 02:40:25 -0700

    I do not know why 2d changed

--- scripts/run_metric_2d.sh
+++ scripts/run_metric_2d.sh
@@ -2,9 +2,8 @@ ROOT_DIR=$(dirname $(dirname "$(readlink -f "$0")"))'/'
 MODEL_DIR=$ROOT_DIR'models'
 THIS_FILE=$(basename "$0")
 
-CONFIG_DIR=$ROOT_DIR'configs/spin_2d.toml' # Change to your config file path
-LOG_DIR=$ROOT_DIR'log/2D/'                 # Change to your log path
-IDENTIFIER='DiGS_2D-2024-10-09-17-31-19'   # change to your desired identifier
+LOG_DIR=$ROOT_DIR'log/2d_curv/'                 # Change to your log path
+IDENTIFIER='spin_curv-2024-10-10-09-50-21'   # change to your desired identifier
 CONFIG_DIR=$LOG_DIR/$IDENTIFIER/*.toml
 EXP_DIR=$LOG_DIR$IDENTIFIER/
 
