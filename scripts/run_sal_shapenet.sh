2a568a7701799fdc0d1b0c97a35e886d6e95330e
Sirui Tao <dylantaosirui@outlook.com>
2024-10-22 12:15:49 -0700

    update sal code to resume training

--- scripts/run_sal_shapenet.sh
+++ scripts/run_sal_shapenet.sh
@@ -1,8 +1,8 @@
 ROOT_DIR=$(dirname $(dirname "$(readlink -f "$0")"))'/'
 MODEL_DIR=$ROOT_DIR'models'
 THIS_FILE=$(basename "$0")
-TIMESTAMP=$(date +"-%Y-%m-%d-%H-%M-%S")
-IDENTIFIER='SAL' # change to your desired identifier
+# TIMESTAMP=$(date +"-%Y-%m-%d-%H-%M-%S")
+IDENTIFIER='SAL-2024-10-20-15-22-38' # change to your desired identifier
 
 CONFIG_DIR=$ROOT_DIR"configs/sal_shapenet.toml" # change to your config file path
 DATASET_DIR=$ROOT_DIR'data/NSP_dataset/'    # change to your dataset path
@@ -12,8 +12,8 @@ mkdir -p $EXP_DIR
 cp -r scripts/$THIS_FILE $EXP_DIR # Copy this script to the experiment directory
 cp -r $CONFIG_DIR $EXP_DIR        # Copy the config file to the experiment directory
 
-for SHAPE_NAME in 'airplane' 'bench' 'cabinet' 'car' 'chair' 'display' 'lamp' 'loudspeaker' 'rifle' 'sofa' 'table' 'telephone' 'watercraft' ; do # use the shapes you want in the dataset
-# for SHAPE_NAME in 'airplane' ; do # use the shapes you want in the dataset
+# for SHAPE_NAME in 'airplane' 'bench' 'cabinet' 'car' 'chair' 'display' 'lamp' 'loudspeaker' 'rifle' 'sofa' 'table' 'telephone' 'watercraft' ; do # use the shapes you want in the dataset
+for SHAPE_NAME in 'chair' 'display' 'lamp' 'loudspeaker' 'rifle' 'sofa' 'table' 'telephone' 'watercraft' ; do # use the shapes you want in the dataset
     FOLDER_DIR=${DATASET_DIR}/$SHAPE_NAME/
     echo $SHAPE_NAME
     # for FILE_NAME in 'd1b15263933da857784a45ea6efa1d77.ply'; do # use the scans you want
